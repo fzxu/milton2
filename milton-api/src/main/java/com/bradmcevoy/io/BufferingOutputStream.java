a3e48f1db90d25e04b007daf1b9ddd7cc1cd9ca7
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2009-09-30 10:49:11 +0000

    remove unused import

--- milton-api/src/main/java/com/bradmcevoy/io/BufferingOutputStream.java
+++ milton-api/src/main/java/com/bradmcevoy/io/BufferingOutputStream.java
@@ -5,7 +5,6 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
