bd0cda9c2c3533d0562ffa621c5e125b656a8999
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2009-11-02 11:19:57 +0000

    add author

--- milton-servlet/src/main/java/com/bradmcevoy/http/Initable.java
+++ milton-servlet/src/main/java/com/bradmcevoy/http/Initable.java
@@ -1,5 +1,9 @@
 package com.bradmcevoy.http;
 
+/**
+ *
+ * @author brad
+ */
 public interface Initable {
     void init(ApplicationConfig config, HttpManager manager);
     void destroy(HttpManager manager);
