1f416f38c629b5d65687daf689469e600cc503e1
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2009-11-11 10:27:42 +0000

    MIL-38 - removed dependency on proprietry sun class

--- milton-api/src/main/java/com/bradmcevoy/http/Utils.java
+++ milton-api/src/main/java/com/bradmcevoy/http/Utils.java
@@ -1,16 +1,14 @@
 package com.bradmcevoy.http;
 
+import com.bradmcevoy.common.Path;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
-import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
 import java.util.Calendar;
 import java.util.Date;
-
-import sun.nio.cs.ThreadLocalCoders;
-
-import com.bradmcevoy.common.Path;
 
 public class Utils {
 
@@ -19,8 +17,8 @@ public class Utils {
         '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
     };
 
-    public static Resource findChild(Resource parent, Path path) {
-        return _findChild(parent, path.getParts(), 0);
+    public static Resource findChild( Resource parent, Path path ) {
+        return _findChild( parent, path.getParts(), 0 );
     }
 
     /**
@@ -32,34 +30,33 @@ public class Utils {
      * @param href
      * @return
      */
-    public static String decodePath(String href) {
-        href = href.replace( "[", "%5B").replace( "]", "%5D");
+    public static String decodePath( String href ) {
+        href = href.replace( "[", "%5B" ).replace( "]", "%5D" );
         try {
-            if( href.startsWith("/")) {
-                URI uri = new URI("http://anything.com" + href);
-                return  uri.getPath();
+            if( href.startsWith( "/" ) ) {
+                URI uri = new URI( "http://anything.com" + href );
+                return uri.getPath();
             } else {
-                URI uri = new URI("http://anything.com/" + href);
+                URI uri = new URI( "http://anything.com/" + href );
                 String s = uri.getPath();
-                return s.substring(1);
+                return s.substring( 1 );
             }
-        } catch (URISyntaxException ex) {
-            throw new RuntimeException(ex);
+        } catch( URISyntaxException ex ) {
+            throw new RuntimeException( ex );
         }
     }
 
-
-    private static Resource _findChild(Resource parent, String[] arr, int i) {
-        if (parent instanceof CollectionResource) {
+    private static Resource _findChild( Resource parent, String[] arr, int i ) {
+        if( parent instanceof CollectionResource ) {
             CollectionResource col = (CollectionResource) parent;
             String childName = arr[i];
 
-            Resource child = col.child(childName);
-            if (child == null) {
+            Resource child = col.child( childName );
+            if( child == null ) {
                 return null;
             } else {
-                if (i < arr.length - 1) {
-                    return _findChild(child, arr, i + 1);
+                if( i < arr.length - 1 ) {
+                    return _findChild( child, arr, i + 1 );
                 } else {
                     return child;
                 }
@@ -73,32 +70,31 @@ public class Utils {
         return new Date();
     }
 
-    public static Date addSeconds(Date dt, long seconds) {
-        return addSeconds(dt, (int) seconds);
+    public static Date addSeconds( Date dt, long seconds ) {
+        return addSeconds( dt, (int) seconds );
     }
 
-    public static Date addSeconds(Date dt, int seconds) {
+    public static Date addSeconds( Date dt, int seconds ) {
         Calendar cal = Calendar.getInstance();
-        cal.setTime(dt);
-        cal.add(Calendar.SECOND, seconds);
+        cal.setTime( dt );
+        cal.add( Calendar.SECOND, seconds );
         return cal.getTime();
     }
 
-    public static String getProtocol(String url) {
-        String protocol = url.substring(0, url.indexOf(":"));
+    public static String getProtocol( String url ) {
+        String protocol = url.substring( 0, url.indexOf( ":" ) );
         return protocol;
     }
 
-    public static String escapeXml(String s) {
-        s = s.replaceAll("\"", "&quot;");
-        s = s.replaceAll("&", "&amp;");
-        s = s.replaceAll("'", "&apos;");
-        s = s.replaceAll("<", "&lt;");
-        s = s.replaceAll(">", "&gt;");
+    public static String escapeXml( String s ) {
+        s = s.replaceAll( "\"", "&quot;" );
+        s = s.replaceAll( "&", "&amp;" );
+        s = s.replaceAll( "'", "&apos;" );
+        s = s.replaceAll( "<", "&lt;" );
+        s = s.replaceAll( ">", "&gt;" );
 //        s = s.replaceAll("æ", "ae");
         return s;
     }
-
 
     /**
      * this is a modified verion of java.net.URI.encode(s)
@@ -109,60 +105,58 @@ public class Utils {
      * this method should be applied only to parts of a URL, not the whole
      * URL as forward slashes, semi-colons etc will be encoded
      *
+     * by "part of url" i mean the bits between slashes
+     *
      * @param s
      * @return
      */
-    public static String percentEncode(String s) {
+    public static String percentEncode( String s ) {
         int n = s.length();
-        if (n == 0) {
+        if( n == 0 ) {
             return s;
         }
 
         // First check whether we actually need to encode
-        for (int i = 0;;) {
+        for( int i = 0;; ) {
             char b = s.charAt( i );
-            if (b >= '\u0080' || b <= (char)48 || isSquareBracket( b)) {
+            if( b >= '\u0080' || b <= (char) 48 || isSquareBracket( b ) ) {
                 break;
             }
-            if (++i >= n) {
+            if( ++i >= n ) {
                 return s;
             }
         }
-        
-        String ns = normalize(s);
+
+        String ns = normalize( s );
         ByteBuffer bb = null;
-        try {
-            bb = ThreadLocalCoders.encoderFor("UTF-8").encode(CharBuffer.wrap(ns));
-        } catch (CharacterCodingException x) {
-            assert false;
-        }
+        bb = Charset.forName( "UTF-8" ).encode( CharBuffer.wrap( ns ) );
 
         StringBuffer sb = new StringBuffer();
-        while (bb.hasRemaining()) {
+        while( bb.hasRemaining() ) {
             int b = bb.get() & 0xff;
-            if ((b >= 0x80 || b < (char)48 || isSquareBracket(b)) && (b != '.' && b != '-')) {
-                appendEscape(sb, (byte) b);
+            if( ( b >= 0x80 || b < (char) 48 || isSquareBracket( b ) ) && ( b != '.' && b != '-' ) ) {
+                appendEscape( sb, (byte) b );
             } else {
-                sb.append((char) b);
+                sb.append( (char) b );
             }
         }
         return sb.toString();
     }
 
-    private static boolean isSquareBracket(int b) {
+    private static boolean isSquareBracket( int b ) {
         return b == 0x5B || b == 0x5D;
     }
 
-    private static void appendEscape(StringBuffer sb, byte b) {
-        sb.append('%');
-        sb.append(hexDigits[(b >> 4) & 0x0f]);
-        sb.append(hexDigits[(b >> 0) & 0x0f]);
+    private static void appendEscape( StringBuffer sb, byte b ) {
+        sb.append( '%' );
+        sb.append( hexDigits[( b >> 4 ) & 0x0f] );
+        sb.append( hexDigits[( b >> 0 ) & 0x0f] );
     }
 
-    public static Date mostRecent(Date ... dates) {
+    public static Date mostRecent( Date... dates ) {
         if( dates == null || dates.length == 0 ) return null;
         Date recent = dates[0];
-        for( Date dt : dates) {
+        for( Date dt : dates ) {
             if( dt.getTime() > recent.getTime() ) recent = dt;
         }
         return recent;
@@ -180,7 +174,7 @@ public class Utils {
      * @param s
      * @return
      */
-    private static String normalize(String s) {
+    private static String normalize( String s ) {
         //return Normalizer.normalize(s, Normalizer.Form.NFC);
         return s;
     }
