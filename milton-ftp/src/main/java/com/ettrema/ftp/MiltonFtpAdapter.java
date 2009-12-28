963d0f5710800a7c6b6b6ed430e06c5bd1ccfa7a
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2009-12-28 03:01:20 +0000

    enable simple port configuration of ftp

--- milton-ftp/src/main/java/com/ettrema/ftp/MiltonFtpAdapter.java
+++ milton-ftp/src/main/java/com/ettrema/ftp/MiltonFtpAdapter.java
@@ -32,30 +32,84 @@ import org.slf4j.LoggerFactory;
 public class MiltonFtpAdapter implements FileSystemFactory {
 
     private static final Logger log = LoggerFactory.getLogger( MiltonFtpAdapter.class );
-    private FtpServerFactory serverFactory;
     private final ResourceFactory resourceFactory;
     private final FtpServer server;
+
+    /**
+     * Just sets dependencies. Does NOT start the server
+     *
+     * @param resourceFactory
+     * @param server
+     */
+    public MiltonFtpAdapter( ResourceFactory resourceFactory, FtpServer server ) {
+        this.resourceFactory = resourceFactory;
+        this.server = server;
+    }
+
+
 
     public MiltonFtpAdapter( ResourceFactory wrapped, UserManager userManager ) throws FtpException {
         this(wrapped, userManager, null );
     }
 
+    /**
+     * Creates and starts the FTP server on port 21
+     *
+     * @param wrapped
+     * @param userManager
+     * @param actionListener
+     * @throws FtpException
+     */
     public MiltonFtpAdapter( ResourceFactory wrapped, UserManager userManager,FtpActionListener actionListener ) throws FtpException {
+        this(wrapped, userManager, actionListener, 21, true);
+    }
+
+    /**
+     * Creates and starts the FTP server on the given port
+     * 
+     * @param wrapped
+     * @param userManager
+     * @param port
+     * @throws FtpException
+     */
+    public MiltonFtpAdapter( ResourceFactory wrapped, UserManager userManager, int port ) throws FtpException {
+        this(wrapped, userManager, null, port, true );
+    }
+
+
+    /**
+     * Creates and optionally starts the server
+     *
+     * @param wrapped
+     * @param userManager
+     * @param actionListener
+     * @param port
+     * @param autoStart - whether or not to start the server
+     * @throws FtpException
+     */
+    public MiltonFtpAdapter( ResourceFactory wrapped, UserManager userManager,FtpActionListener actionListener, int port, boolean autoStart ) throws FtpException {
         log.debug("creating MiltonFtpAdapter.2");
         this.resourceFactory = wrapped;
-
-        serverFactory = new FtpServerFactory();
+        FtpServerFactory serverFactory = new FtpServerFactory();
+        ListenerFactory factory;
         if( actionListener != null ) {
             log.debug( "using customised milton listener factory");
             MiltonFtpHandler ftpHandler = new MiltonFtpHandler(new DefaultFtpHandler(),actionListener);
-            ListenerFactory factory = new MiltonListenerFactory(ftpHandler);
-            serverFactory.addListener("default", factory.createListener());
+            factory = new MiltonListenerFactory(ftpHandler);
+        } else {
+            factory = new ListenerFactory();
         }
+        factory.setPort( port );
+        serverFactory.addListener("default", factory.createListener());
+
         
         serverFactory.setFileSystem( this );
         serverFactory.setUserManager( userManager);
         server = serverFactory.createServer();
-        server.start();
+        if( autoStart) {
+            log.debug( "starting the FTP server on port: " + port);
+            server.start();
+        }
     }
 
     public Resource getResource( Path path, String host ) {
