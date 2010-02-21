d10619e097357b36dbde2451cbc4e5df64ff5171
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2010-02-21 09:33:49 +0000

    add reason, for use in some response handlers

--- milton-api/src/main/java/com/bradmcevoy/http/exceptions/BadRequestException.java
+++ milton-api/src/main/java/com/bradmcevoy/http/exceptions/BadRequestException.java
@@ -9,8 +9,26 @@ import com.bradmcevoy.http.Resource;
 public class BadRequestException extends MiltonException {
     private static final long serialVersionUID = 1L;
 
+    private final String reason;
+
     public BadRequestException(Resource r) {
         super(r);
+        this.reason = null;
     }
+
+    public BadRequestException(Resource r, String reason) {
+        super(r);
+        this.reason = reason;
+    }
+
+    /**
+     * Optional property, which describe the cause of the exception
+     * @return
+     */
+    public String getReason() {
+        return reason;
+    }
+
+
 
 }
