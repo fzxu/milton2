90e8b780bdd39b944bbdd3ed9390fbfc450f0d51
DANNES <DANNES@89edebda-9267-40c1-a934-9420f7305bf7>
2010-08-14 17:33:47 +0000

    [ignore]  update of javadoc (markup)

--- milton-api/src/main/java/com/bradmcevoy/http/LockingCollectionResource.java
+++ milton-api/src/main/java/com/bradmcevoy/http/LockingCollectionResource.java
@@ -4,29 +4,29 @@ import com.bradmcevoy.http.exceptions.NotAuthorizedException;
 
 /**
  * A collection which allows locking "unmapped resources". This means that a LOCK
- * method can effectively create an empty resource which is immediately locked
- * 
- * Implement this in conjunction with LockableResource on child resources to fully support locking.
- *
- * Note that this interface now extends LockableResource because collection resources
- * need to implement both in most cases.
- *
+ * method can effectively create an empty resource which is immediately locked.
+ * Implement this in conjunction with {@see LockableResource} on child resources to fully
+ * support locking.
+ * <P/>
+ * <I>Note that this interface now extends {@see LockableResource} because collection resources
+ * need to implement both in most cases.</I>
+ * <P/>
  * If, however, you don't want your collection resources to be lockable, just
- * implement ConditionalCompatibleResource
- * 
- * See - http://www.ettrema.com:8080/browse/MIL-14
- * 
+ * implement {@see ConditionalCompatibleResource}.
+ * <P/>
+ * See <A HREF="http://www.ettrema.com:8080/browse/MIL-14">http://www.ettrema.com:8080/browse/MIL-14</A>.
+ * <P/>
  * @author brad
  */
 public interface  LockingCollectionResource extends CollectionResource, LockableResource {
     
     /**
      * Create an empty non-collection resource of the given name and immediately
-     *  lock it
-     *
+     * lock it.
+     * <P/>
      * It is suggested that the implementor have a specific Resource class to act
      * as the lock null resource. You should consider using the LockNullResource
-     * interface
+     * interface.
      * 
      * @param name - the name of the resource to create
      * @param timeout - in seconds
