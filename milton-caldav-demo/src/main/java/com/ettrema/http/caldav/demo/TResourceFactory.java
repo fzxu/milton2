dc3925e7b6bc341365db09a6576ee1752fd2a6af
alex <alex@89edebda-9267-40c1-a934-9420f7305bf7>
2010-12-22 00:53:04 +0000

    Attempt at improving the resource finding (Not complete)

--- milton-caldav-demo/src/main/java/com/ettrema/http/caldav/demo/TResourceFactory.java
+++ milton-caldav-demo/src/main/java/com/ettrema/http/caldav/demo/TResourceFactory.java
@@ -1,6 +1,7 @@
 package com.ettrema.http.caldav.demo;
 
 import com.bradmcevoy.common.Path;
+import com.bradmcevoy.http.CollectionResource;
 import com.bradmcevoy.http.Resource;
 import com.bradmcevoy.http.ResourceFactory;
 
@@ -19,28 +20,44 @@ public class TResourceFactory implements ResourceFactory {
     public Resource getResource( String host, String url ) {
         log.debug( "getResource: url: " + url );
         Path path = Path.path( url );
-        Resource r = find( path );
+        Resource r = find( path, ROOT );
         log.debug( "_found: " + r + " for url: "+url+" and path: "+path);
         return r;
     }
 
-    private Resource find( Path path ) {
-        if( isRoot( path ) ) return ROOT;
-        Resource r = find( path.getParent() );
-        if( r == null ) return null;
-        if( r instanceof TFolderResource ) {
-            TFolderResource folder = (TFolderResource) r;
-            for( Resource rChild : folder.getChildren() ) {
-                Resource r2 = (Resource) rChild;
-                if( r2.getName().equals( path.getName() ) ) {
-                    log.debug( "RESOURCE FOUND : " + r2.getName() + " - " + path.getName());
-                    return r2;
-                } else {
-                    log.debug( "IS NOT: " + r2.getName() + " - " + path.getName());
-                }
+    private Resource find( Path path, Resource resource) {
+        log.debug("Finding resouce : "+path+" in folder : "+resource.getName());
+          log.debug("Resource : "+resource.getName());
+          log.debug("Path     : "+path.getFirst()+" OR "+path.toString());
+          if (resource.getName().equals(path.getFirst())) {
+          if( resource instanceof CollectionResource )
+          {
+            Path remainder = path.getStripFirst();
+            for (Resource child : ((CollectionResource)resource).getChildren()) {
+              find (remainder, child);
             }
+          }
+          else if ( resource instanceof TResource)
+          {
+            return resource;
+          }
         }
-        log.debug( "not found: " + path );
+
+//        Resource r = find( path.getParent() );
+//        if( r == null ) return null;
+//        if( r instanceof TFolderResource ) {
+//            TFolderResource folder = (TFolderResource) r;
+//            for( Resource rChild : folder.getChildren() ) {
+//                Resource r2 = (Resource) rChild;
+//                if( r2.getName().equals( path.getName() ) ) {
+//                    log.debug( "RESOURCE FOUND : " + r2.getName() + " - " + path.getName());
+//                    return r2;
+//                } else {
+//                    log.debug( "IS NOT: " + r2.getName() + " - " + path.getName());
+//                }
+//            }
+//        }
+//        log.debug( "not found: " + path );
         return null;
     }
 
