57ad28eb8e16f8659b92b25f7462b49a48470dab
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2011-01-31 23:46:54 +0000

    add new RequestEvent, fired before processing any request

--- milton-api/src/main/java/com/bradmcevoy/http/HttpManager.java
+++ milton-api/src/main/java/com/bradmcevoy/http/HttpManager.java
@@ -1,5 +1,8 @@
 package com.bradmcevoy.http;
 
+import com.bradmcevoy.http.exceptions.BadRequestException;
+import com.bradmcevoy.http.exceptions.ConflictException;
+import com.bradmcevoy.http.exceptions.NotAuthorizedException;
 import com.bradmcevoy.http.http11.Http11ResponseHandler;
 import com.bradmcevoy.http.webdav.DefaultWebDavResponseHandler;
 import com.bradmcevoy.http.webdav.WebDavResponseHandler;
@@ -7,6 +10,7 @@ import com.bradmcevoy.property.PropertyHandler;
 import com.bradmcevoy.property.PropertyAuthoriser;
 import com.ettrema.event.EventManager;
 import com.ettrema.event.EventManagerImpl;
+import com.ettrema.event.RequestEvent;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -24,10 +28,10 @@ import org.slf4j.LoggerFactory;
  */
 public class HttpManager {
 
-    private static final Logger log = LoggerFactory.getLogger( HttpManager.class );
+    private static final Logger log = LoggerFactory.getLogger(HttpManager.class);
 
-    public static String decodeUrl( String s ) {
-        return Utils.decodePath( s );
+    public static String decodeUrl(String s) {
+        return Utils.decodePath(s);
     }
     private static final ThreadLocal<Request> tlRequest = new ThreadLocal<Request>();
     private static final ThreadLocal<Response> tlResponse = new ThreadLocal<Response>();
@@ -39,7 +43,6 @@ public class HttpManager {
     public static Response response() {
         return tlResponse.get();
     }
-
     private final ProtocolHandlers handlers;
     private Map<String, Handler> methodHandlers = new ConcurrentHashMap<String, Handler>();
     List<Filter> filters = new ArrayList<Filter>();
@@ -55,42 +58,46 @@ public class HttpManager {
      *
      * @param resourceFactory
      */
-    public HttpManager( ResourceFactory resourceFactory ) {
-        if( resourceFactory == null )
-            throw new NullPointerException( "resourceFactory cannot be null" );
+    public HttpManager(ResourceFactory resourceFactory) {
+        if (resourceFactory == null) {
+            throw new NullPointerException("resourceFactory cannot be null");
+        }
         this.resourceFactory = resourceFactory;
         AuthenticationService authenticationService = new AuthenticationService();
-        DefaultWebDavResponseHandler webdavResponseHandler = new DefaultWebDavResponseHandler( authenticationService );
+        DefaultWebDavResponseHandler webdavResponseHandler = new DefaultWebDavResponseHandler(authenticationService);
         this.responseHandler = webdavResponseHandler;
-        this.handlers = new ProtocolHandlers( webdavResponseHandler, authenticationService );
+        this.handlers = new ProtocolHandlers(webdavResponseHandler, authenticationService);
 
         initHandlers();
     }
 
-    public HttpManager( ResourceFactory resourceFactory, AuthenticationService authenticationService ) {
-        if( resourceFactory == null )
-            throw new NullPointerException( "resourceFactory cannot be null" );
+    public HttpManager(ResourceFactory resourceFactory, AuthenticationService authenticationService) {
+        if (resourceFactory == null) {
+            throw new NullPointerException("resourceFactory cannot be null");
+        }
         this.resourceFactory = resourceFactory;
-        DefaultWebDavResponseHandler webdavResponseHandler = new DefaultWebDavResponseHandler( authenticationService );
+        DefaultWebDavResponseHandler webdavResponseHandler = new DefaultWebDavResponseHandler(authenticationService);
         this.responseHandler = webdavResponseHandler;
-        this.handlers = new ProtocolHandlers( webdavResponseHandler, authenticationService );
+        this.handlers = new ProtocolHandlers(webdavResponseHandler, authenticationService);
 
         initHandlers();
     }
 
-    public HttpManager( ResourceFactory resourceFactory, WebDavResponseHandler responseHandler, AuthenticationService authenticationService ) {
-        if( resourceFactory == null )
-            throw new NullPointerException( "resourceFactory cannot be null" );
+    public HttpManager(ResourceFactory resourceFactory, WebDavResponseHandler responseHandler, AuthenticationService authenticationService) {
+        if (resourceFactory == null) {
+            throw new NullPointerException("resourceFactory cannot be null");
+        }
         this.resourceFactory = resourceFactory;
         this.responseHandler = responseHandler;
-        this.handlers = new ProtocolHandlers( responseHandler, authenticationService );
+        this.handlers = new ProtocolHandlers(responseHandler, authenticationService);
 
         initHandlers();
     }
 
-    public HttpManager( ResourceFactory resourceFactory, WebDavResponseHandler responseHandler, ProtocolHandlers handlers ) {
-        if( resourceFactory == null )
-            throw new NullPointerException( "resourceFactory cannot be null" );
+    public HttpManager(ResourceFactory resourceFactory, WebDavResponseHandler responseHandler, ProtocolHandlers handlers) {
+        if (resourceFactory == null) {
+            throw new NullPointerException("resourceFactory cannot be null");
+        }
         this.resourceFactory = resourceFactory;
         this.responseHandler = responseHandler;
         this.handlers = handlers;
@@ -99,19 +106,19 @@ public class HttpManager {
     }
 
     private void initHandlers() {
-        for( HttpExtension ext : handlers ) {
-            for( Handler h : ext.getHandlers() ) {
-                for( String m : h.getMethods() ) {
-                    this.methodHandlers.put( m, h );
+        for (HttpExtension ext : handlers) {
+            for (Handler h : ext.getHandlers()) {
+                for (String m : h.getMethods()) {
+                    this.methodHandlers.put(m, h);
                 }
             }
         }
         // The standard filter must always be there, its what invokes the main milton processing
-        filters.add( createStandardFilter() );
+        filters.add(createStandardFilter());
     }
 
-    public Handler getMethodHandler( Request.Method m ) {
-        return methodHandlers.get( m.code );
+    public Handler getMethodHandler(Request.Method m) {
+        return methodHandlers.get(m.code);
     }
 
     public ResourceFactory getResourceFactory() {
@@ -122,7 +129,7 @@ public class HttpManager {
         return sessionAuthenticationHandler;
     }
 
-    public void setSessionAuthenticationHandler( SessionAuthenticationHandler sessionAuthenticationHandler ) {
+    public void setSessionAuthenticationHandler(SessionAuthenticationHandler sessionAuthenticationHandler) {
         this.sessionAuthenticationHandler = sessionAuthenticationHandler;
     }
 
@@ -136,20 +143,31 @@ public class HttpManager {
      * 
      */
     @Deprecated
-    public Auth getSessionAuthentication( Request request ) {
-        if( this.sessionAuthenticationHandler == null ) return null;
-        return this.sessionAuthenticationHandler.getSessionAuthentication( request );
+    public Auth getSessionAuthentication(Request request) {
+        if (this.sessionAuthenticationHandler == null) {
+            return null;
+        }
+        return this.sessionAuthenticationHandler.getSessionAuthentication(request);
     }
 
-    public void process( Request request, Response response ) {
-        if( log.isInfoEnabled() ) {
-            log.info( request.getMethod() + " :: " + request.getAbsoluteUrl() + " - " + request.getAbsoluteUrl() );
+    public void process(Request request, Response response) {
+        if (log.isInfoEnabled()) {
+            log.info(request.getMethod() + " :: " + request.getAbsoluteUrl() + " - " + request.getAbsoluteUrl());
         }
-        tlRequest.set( request );
-        tlResponse.set( response );
+        tlRequest.set(request);
+        tlResponse.set(response);
         try {
-            FilterChain chain = new FilterChain( this );
-            chain.process( request, response );
+            fireRequestEvent(request);
+        } catch (ConflictException ex) {
+            responseHandler.respondConflict(null, response, request, null);
+        } catch (BadRequestException ex) {
+            responseHandler.respondBadRequest(null, response, request);
+        } catch (NotAuthorizedException ex) {
+            responseHandler.respondUnauthorised(null, response, request);
+        }
+        try {
+            FilterChain chain = new FilterChain(this);
+            chain.process(request, response);
         } finally {
             tlRequest.remove();
             tlResponse.remove();
@@ -160,53 +178,53 @@ public class HttpManager {
         return new StandardFilter();
     }
 
-    public void addFilter( int pos, Filter filter ) {
-        filters.add( pos, filter );
+    public void addFilter(int pos, Filter filter) {
+        filters.add(pos, filter);
     }
 
-    public void addEventListener( EventListener l ) {
-        eventListeners.add( l );
+    public void addEventListener(EventListener l) {
+        eventListeners.add(l);
     }
 
-    public void removeEventListener( EventListener l ) {
-        eventListeners.remove( l );
+    public void removeEventListener(EventListener l) {
+        eventListeners.remove(l);
     }
 
-    public void onProcessResourceFinish( Request request, Response response, Resource resource, long duration ) {
-        for( EventListener l : eventListeners ) {
-            l.onProcessResourceFinish( request, response, resource, duration );
+    public void onProcessResourceFinish(Request request, Response response, Resource resource, long duration) {
+        for (EventListener l : eventListeners) {
+            l.onProcessResourceFinish(request, response, resource, duration);
         }
     }
 
-    public void onProcessResourceStart( Request request, Response response, Resource resource ) {
-        for( EventListener l : eventListeners ) {
-            l.onProcessResourceStart( request, response, resource );
+    public void onProcessResourceStart(Request request, Response response, Resource resource) {
+        for (EventListener l : eventListeners) {
+            l.onProcessResourceStart(request, response, resource);
         }
     }
 
-    public void onPost( Request request, Response response, Resource resource, Map<String, String> params, Map<String, FileItem> files ) {
-        for( EventListener l : eventListeners ) {
-            l.onPost( request, response, resource, params, files );
+    public void onPost(Request request, Response response, Resource resource, Map<String, String> params, Map<String, FileItem> files) {
+        for (EventListener l : eventListeners) {
+            l.onPost(request, response, resource, params, files);
         }
     }
 
-    public void onGet( Request request, Response response, Resource resource, Map<String, String> params ) {
-        for( EventListener l : eventListeners ) {
-            l.onGet( request, response, resource, params );
+    public void onGet(Request request, Response response, Resource resource, Map<String, String> params) {
+        for (EventListener l : eventListeners) {
+            l.onGet(request, response, resource, params);
         }
     }
 
     public List<Filter> getFilters() {
-        ArrayList<Filter> col = new ArrayList<Filter>( filters );
+        ArrayList<Filter> col = new ArrayList<Filter>(filters);
         return col;
     }
 
-    public void setFilters( List<Filter> filters ) {
+    public void setFilters(List<Filter> filters) {
         this.filters = filters;
-        filters.add( new StandardFilter() );
+        filters.add(new StandardFilter());
     }
 
-    public void setEventListeners( List<EventListener> eventListeners ) {
+    public void setEventListeners(List<EventListener> eventListeners) {
         this.eventListeners = eventListeners;
     }
 
@@ -226,14 +244,14 @@ public class HttpManager {
         return propertyPermissionService;
     }
 
-    public void setPropertyPermissionService( PropertyAuthoriser propertyPermissionService ) {
-        log.trace( "setPropertyPermissionService: " + propertyPermissionService.getClass().getCanonicalName() );
+    public void setPropertyPermissionService(PropertyAuthoriser propertyPermissionService) {
+        log.trace("setPropertyPermissionService: " + propertyPermissionService.getClass().getCanonicalName());
         this.propertyPermissionService = propertyPermissionService;
-        for( Handler h : methodHandlers.values() ) {
-            if( h instanceof PropertyHandler ) {
+        for (Handler h : methodHandlers.values()) {
+            if (h instanceof PropertyHandler) {
                 PropertyHandler ph = (PropertyHandler) h;
-                log.trace( "set propertyPermissionService on: " + ph.getClass().getCanonicalName() );
-                ph.setPermissionService( propertyPermissionService );
+                log.trace("set propertyPermissionService on: " + ph.getClass().getCanonicalName());
+                ph.setPermissionService(propertyPermissionService);
             }
         }
     }
@@ -242,17 +260,22 @@ public class HttpManager {
         return handlers.isEnableExpectContinue();
     }
 
-    public void setEnableExpectContinue( boolean enableExpectContinue ) {
-        handlers.setEnableExpectContinue( enableExpectContinue );
+    public void setEnableExpectContinue(boolean enableExpectContinue) {
+        handlers.setEnableExpectContinue(enableExpectContinue);
     }
 
     public EventManager getEventManager() {
         return eventManager;
     }
 
-    public void setEventManager( EventManager eventManager ) {
+    public void setEventManager(EventManager eventManager) {
         this.eventManager = eventManager;
     }
 
-
+    private void fireRequestEvent(Request request) throws ConflictException, BadRequestException, NotAuthorizedException {
+        if (eventManager == null) {
+            return;
+        }
+        eventManager.fireEvent(new RequestEvent(request)); 
+    }
 }
