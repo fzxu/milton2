ff318f54b149d888e00d1be7778456404e048947
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2011-12-03 19:58:44 +0000

    adjust white space in href list output

--- milton-api/src/main/java/com/bradmcevoy/http/values/HrefListValueWriter.java
+++ milton-api/src/main/java/com/bradmcevoy/http/values/HrefListValueWriter.java
@@ -12,27 +12,28 @@ import java.util.Map;
  * @author brad
  */
 public class HrefListValueWriter implements ValueWriter {
-	@Override
-    public boolean supports( String nsUri, String localName, Class c ) {
-        return HrefList.class.isAssignableFrom( c );
-    }
 
 	@Override
-    public void writeValue( XmlWriter writer, String nsUri, String prefix, String localName, Object val, String href, Map<String, String> nsPrefixes ) {
-      writer.open(prefix, localName);
-      HrefList list = (HrefList) val;
-      if( list != null ) {
-          for( String s : list) {
-              Element hrefEl = writer.begin( "D:href" ).open(false);
-              hrefEl.writeText( s );
-              hrefEl.close();
-          }
-      }
-      writer.close(prefix, localName);
-    }
+	public boolean supports(String nsUri, String localName, Class c) {
+		return HrefList.class.isAssignableFrom(c);
+	}
 
 	@Override
-    public Object parse( String namespaceURI, String localPart, String value ) {
-        throw new UnsupportedOperationException( "Not supported yet." );
-    }
+	public void writeValue(XmlWriter writer, String nsUri, String prefix, String localName, Object val, String href, Map<String, String> nsPrefixes) {
+		Element outerEl = writer.begin(prefix, localName).open();
+		HrefList list = (HrefList) val;
+		if (list != null) {
+			for (String s : list) {
+				Element hrefEl = writer.begin("D:href").open(false);
+				hrefEl.writeText(s);
+				hrefEl.close();
+			}
+		}
+		outerEl.close();
+	}
+
+	@Override
+	public Object parse(String namespaceURI, String localPart, String value) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
