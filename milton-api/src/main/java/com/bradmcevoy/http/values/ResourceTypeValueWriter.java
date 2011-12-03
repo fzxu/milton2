ae8a11c83f5df9899378e021102861aba78bb1d2
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2011-12-03 09:01:23 +0000

    fix for caldav resource types, output namespace uri even when namespace is not found in requested properties

--- milton-api/src/main/java/com/bradmcevoy/http/values/ResourceTypeValueWriter.java
+++ milton-api/src/main/java/com/bradmcevoy/http/values/ResourceTypeValueWriter.java
@@ -10,29 +10,38 @@ import org.slf4j.LoggerFactory;
 
 public class ResourceTypeValueWriter implements ValueWriter {
 
-    private static final Logger log = LoggerFactory.getLogger( ResourceTypeValueWriter.class );
+	private static final Logger log = LoggerFactory.getLogger(ResourceTypeValueWriter.class);
 
-    public boolean supports( String nsUri, String localName, Class c ) {
-        return localName.equals( "resourcetype" );
-    }
+	@Override
+	public boolean supports(String nsUri, String localName, Class c) {
+		return localName.equals("resourcetype");
+	}
 
-    public void writeValue( XmlWriter writer, String nsUri, String prefix, String localName, Object val, String href, Map<String, String> nsPrefixes ) {
-        List<QName> list = (List<QName>) val;
-        if( list != null && list.size() > 0 ) {
-            Element rt = writer.begin( prefix, localName );
-            for( QName name : list ) {
-                String childNsUri = name.getNamespaceURI();
-                String childPrefix = nsPrefixes.get( childNsUri );
-                // don't write a new line - see http://www.ettrema.com:8080/browse/MIL-83
-                rt.begin( childPrefix, name.getLocalPart() ).noContent(false);
-            }
-            rt.close();
-        } else {
-            writer.writeProperty( prefix, localName );
-        }
-    }
+	@Override
+	public void writeValue(XmlWriter writer, String nsUri, String prefix, String localName, Object val, String href, Map<String, String> nsPrefixes) {
+		List<QName> list = (List<QName>) val;
+		if (list != null && list.size() > 0) {
+			Element rt = writer.begin(prefix, localName);
+			for (QName name : list) {
+				String childNsUri = name.getNamespaceURI();
+				String childPrefix = nsPrefixes.get(childNsUri);
+				// might be null if the namespace is on a value qname but not a property (eg caldav resource type)
+				// so if null write the full uri
+				if (childPrefix == null) {
+					rt.begin(childNsUri, childPrefix, name.getLocalPart()).noContent(false);
+				} else {
+					// don't write a new line - see http://www.ettrema.com:8080/browse/MIL-83
+					rt.begin(childPrefix, name.getLocalPart()).noContent(false);
+				}
+			}
+			rt.close();
+		} else {
+			writer.writeProperty(prefix, localName);
+		}
+	}
 
-    public Object parse( String namespaceURI, String localPart, String value ) {
-        throw new UnsupportedOperationException( "Not supported yet." );
-    }
+	@Override
+	public Object parse(String namespaceURI, String localPart, String value) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 }
