e17aa0e21791de09b7dd498ccaa4b200be89e37c
brad <brad@89edebda-9267-40c1-a934-9420f7305bf7>
2012-01-24 22:06:44 +0000

    beginning work on a DAV proxy. It a webdav server which provides access to multiple other dav servers

--- milton-proxy/src/main/java/com/gnostech/webdav/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.gnostech.webdav.dao;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class UserDAO {
-	private static final Logger log = LoggerFactory.getLogger(UserDAO.class);
-	
-}
